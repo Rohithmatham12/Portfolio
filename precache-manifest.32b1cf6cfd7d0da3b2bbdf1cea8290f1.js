bed5cf653031511cdf42b6610dd31cef10bc1522
Rohithmatham12 <81304757+Rohithmatham12@users.noreply.github.com>
2024-05-29 23:16:08 -0700

    Updates

---
+++ precache-manifest.32b1cf6cfd7d0da3b2bbdf1cea8290f1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9643b4e5a8eac8f09b78560d3034f18",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "efbb057ae9140a052627",
+    "url": "/Portfolio/static/css/main.8eb327a1.chunk.css"
+  },
+  {
+    "revision": "f5619bc557eaff3819b9",
+    "url": "/Portfolio/static/js/2.cc2186da.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/Portfolio/static/js/2.cc2186da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efbb057ae9140a052627",
+    "url": "/Portfolio/static/js/main.4a9db0d5.chunk.js"
+  },
+  {
+    "revision": "5edbb31abbe4f9c5c9e5",
+    "url": "/Portfolio/static/js/runtime-main.4144f624.js"
+  },
+  {
+    "revision": "862416bd2f90d2aa2ccad297eec200b4",
+    "url": "/Portfolio/static/media/Rohith-Cv.862416bd.pdf"
+  },
+  {
+    "revision": "ba1a9d39b4cb6766d16bfb6613dab75f",
+    "url": "/Portfolio/static/media/about.ba1a9d39.jpeg"
+  },
+  {
+    "revision": "ba1a9d39b4cb6766d16bfb6613dab75f",
+    "url": "/Portfolio/static/media/profile.ba1a9d39.jpeg"
+  },
+  {
+    "revision": "7b93cdc0a0eeb9864563216bd2fad722",
+    "url": "/Portfolio/static/media/work1.7b93cdc0.png"
+  },
+  {
+    "revision": "869670aad7157e19dce24300090e6676",
+    "url": "/Portfolio/static/media/work2.869670aa.png"
+  },
+  {
+    "revision": "52eb46b371d456bca09e17c0dff69e15",
+    "url": "/Portfolio/static/media/work3.52eb46b3.png"
+  },
+  {
+    "revision": "b1886c028ea1d2797323c565e0bfab0c",
+    "url": "/Portfolio/static/media/work4.b1886c02.png"
+  },
+  {
+    "revision": "6fbdb534682925aaf5e77e26c82affea",
+    "url": "/Portfolio/static/media/work5.6fbdb534.png"
+  }
+]);
