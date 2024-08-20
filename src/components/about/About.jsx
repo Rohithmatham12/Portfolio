1ff0dbb4e91ba09f67f0c105179d16030b321296
Rohithmatham12 <rohithmatham@gmail.com>
2024-08-19 20:05:44 -0700

    changed the path

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./about.css";
-import AboutImg from "/Users/rohithmatam/Developer/Portfolio/strike/src/assets/about.jpeg";
-import CV from "/Users/rohithmatam/Developer/Portfolio/strike/src/assets/Rohith-Cv.pdf";
+import AboutImg from "/Users/rohithmatam/Portfolio/src/assets/about.jpeg";
+import CV from "/Users/rohithmatam/Portfolio/src/assets/Rohith-Cv.pdf";
 import Info from './Info';
 
 const About = () => {
