7d516b75a857f1402cf4af811e5d690881164d4e
Rohithmatham12 <rohithmatham@gmail.com>
2024-08-19 20:08:48 -0700

    changed the path in data.jsx

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,8 +1,8 @@
-import Work1 from "src/assets/work1.png";
-import Work2 from "src/assets/work2.png";
-import Work3 from "src/assets/work3.png";
-import Work4 from "src/assets/work4.png";
-import Work5 from "src/assets/work5.png";
+import Work1 from "../../assets/work1.png";
+import Work2 from "../../assets/work2.png";
+import Work3 from "../../assets/work3.png";
+import Work4 from "../../assets/work4.png";
+import Work5 from "../../assets/work5.png";
 
 export const projectsData = [
     {
