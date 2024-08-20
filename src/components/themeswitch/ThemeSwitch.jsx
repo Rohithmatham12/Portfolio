59765b14559f9291f382535739887946a44f6628
Rohithmatham12 <rohithmatham@gmail.com>
2024-08-19 21:11:44 -0700

    editing the app.js

--- src/components/themeswitch/ThemeSwitch.jsx
+++ src/components/themeswitch/ThemeSwitch.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import "./ThemeSwitch.css";
 
 const ThemeSwitch = ({ isDarkMode, toggleTheme }) => {
-  return (
-    <button onClick={toggleTheme} className="nav__btns theme-switch">
-      <i className={`uil ${isDarkMode ? 'uil-sun' : 'uil-moon'} nav__icon`}></i>
-      <span className="theme-switch__text">{isDarkMode ? 'Light Mode' : 'Dark Mode'}</span>
-    </button>
-  );
-};
+    return (
+      <button onClick={toggleTheme} className="theme-switch nav__link">
+        <i className={`uil ${isDarkMode ? 'uil-sun' : 'uil-moon'}`}></i>
+      </button>
+    );
+  };
 
 export default ThemeSwitch;
